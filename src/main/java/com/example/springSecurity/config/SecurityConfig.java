6ae1ba2b1511f8f22cfadb58b7268bf43f727a18
pyounani <yndbsk9372@gmail.com>
2024-04-03 12:39:45 +0900

    [feat] 인가 설정을 위한 클래스 구현

---
+++ src/main/java/com/example/springSecurity/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.springSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        // 순서대로 작동하기 때문에 순서에 주의해서 코드를 짜야 한다.
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()   // 로그인한 사용자만
+                );
+
+        return http.build();
+    }
+
+}
+
